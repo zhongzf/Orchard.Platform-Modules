dd9113eee7a951da4fae2a8e95c1ff71781b8394
zhongzf <zhongzf@126.com>
2014-02-01 13:07:16 +0800

    RaisingStudio.Contents.RepositoryFactory

---
+++ Modules/RaisingStudio.Contents.RepositoryFactory/Services/IContentsRepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaisingStudio.Contents.RepositoryFactory.Services
+{
+    public interface IContentsRepositoryFactory : IDependency
+    {
+        IContentsRepository<T> GetRepository<T>() where T : class, new();
+    }
+}
