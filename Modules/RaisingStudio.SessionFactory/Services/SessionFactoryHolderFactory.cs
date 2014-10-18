fe61fea76606076dad178af660492aa2c51e4370
zhongzf <zhongzf@126.com>
2014-10-18 21:47:53 +0800

    upgrade to support orchard v1.8.1
    1. upgrade to support orchard v1.8.1
    2. cache the SessionFactory

--- Modules/RaisingStudio.SessionFactory/Services/SessionFactoryHolderFactory.cs
+++ Modules/RaisingStudio.SessionFactory/Services/SessionFactoryHolderFactory.cs
@@ -9,17 +9,25 @@ using Orchard.Environment.ShellBuilders.Models;
 using Orchard.FileSystems.AppData;
 using Orchard.Localization;
 using Orchard.Logging;
+using Orchard.Security;
 using RaisingStudio.SessionFactory.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
-
+using Orchard.Utility.Extensions;
+using System.IO;
+using System.Configuration;
+using Orchard.Caching;
+using System.Collections.Concurrent;
 
 namespace RaisingStudio.SessionFactory.Services
 {
     public class SessionFactoryHolderFactory : ISessionFactoryHolderFactory
     {
+        public const string CacheName = "RaisingStudio.SessionFactory.SessionFactoryHolder.Cache";
+
         private readonly ShellSettings _shellSettings;
         private readonly ShellBlueprint _shellBlueprint;
         private readonly IHostEnvironment _hostEnvironment;
@@ -29,6 +37,8 @@ namespace RaisingStudio.SessionFactory.Services
         private readonly IAppDataFolder _appDataFolder;
         private readonly ISessionConfigurationCache _sessionConfigurationCache;
         private readonly IRepository<ConnectionsRecord> _connectionsRecordRepository;
+        private readonly IEncryptionService _encryptionService;
+        private readonly ICacheManager _cacheManager;
 
         public SessionFactoryHolderFactory(
             ShellSettings shellSettings,
@@ -39,7 +49,9 @@ namespace RaisingStudio.SessionFactory.Services
             IHostEnvironment hostEnvironment,
             IDatabaseCacheConfiguration cacheConfiguration,
             Func<IEnumerable<ISessionConfigurationEvents>> configurers,
-            IRepository<ConnectionsRecord> connectionsRecordRepository)
+            IRepository<ConnectionsRecord> connectionsRecordRepository,
+            IEncryptionService encryptionService,
+            ICacheManager cacheManager)
         {
             _shellSettings = shellSettings;
             _shellBlueprint = shellBlueprint;
@@ -50,6 +62,8 @@ namespace RaisingStudio.SessionFactory.Services
             _cacheConfiguration = cacheConfiguration;
             _configurers = configurers;
             _connectionsRecordRepository = connectionsRecordRepository;
+            _encryptionService = encryptionService;
+            _cacheManager = cacheManager;
 
             T = NullLocalizer.Instance;
             Logger = NullLogger.Instance;
@@ -74,7 +88,22 @@ namespace RaisingStudio.SessionFactory.Services
                 connectionString);
         }
 
+
         public ICustomSessionFactoryHolder CreateSessionFactoryHolder(string name)
+        {
+            // cache
+            var sessionFactoryHolderCache = _cacheManager.Get(CacheName, ctx =>
+            {
+                return new ConcurrentDictionary<string, ICustomSessionFactoryHolder>();
+            });
+            var sessionFactoryHolder = sessionFactoryHolderCache.GetOrAdd(CacheName, key =>
+            {
+                return InternalCreateSessionFactoryHolder(name);
+            });
+            return sessionFactoryHolder;
+        }
+
+        protected virtual ICustomSessionFactoryHolder InternalCreateSessionFactoryHolder(string name)
         {
             var connectionsRecord = _connectionsRecordRepository.Fetch(c => c.Name == name).SingleOrDefault();
             return CreateSessionFactoryHolder(connectionsRecord.Provider, connectionsRecord.ConnectionString);
